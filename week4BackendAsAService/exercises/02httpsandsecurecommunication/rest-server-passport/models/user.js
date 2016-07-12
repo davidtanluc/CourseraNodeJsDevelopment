30c4b793d79b2461956310ebc2990855b4c710fe
David Tan <dtan@luc.edu>
2016-07-12 16:58:06 -0500

    add facebook passport for week 4 exercise

--- week4BackendAsAService/exercises/02httpsandsecurecommunication/rest-server-passport/models/user.js
+++ week4BackendAsAService/exercises/02httpsandsecurecommunication/rest-server-passport/models/user.js
@@ -5,8 +5,6 @@ var passportLocalMongoose = require('passport-local-mongoose');
 var User = new Schema({
     username: String,
     password: String,
-    OauthId: String,
-    OauthToken: String,
     firstname: {
         type: String,
         default: ''
--- week4BackendAsAService/exercises/03oauthanduserauthentication/rest-server-passport/app.js
+++ week4BackendAsAService/exercises/03oauthanduserauthentication/rest-server-passport/app.js
@@ -6,8 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-//https://github.com/jaredhanson/passport-local
+//var LocalStrategy = require('passport-local').Strategy;
+var authenticate = require('./authenticate');
 
 var config = require('./config');
 
@@ -48,11 +48,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // passport config
-var User = require('./models/user');
-app.use(passport.initialize());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// var User = require('./models/user');
+// app.use(passport.initialize());
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
